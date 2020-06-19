bbaac4faa51b03bf04f47ecc87c40c19cf86060a
nilov <nilov@glavweb.ru>
2020-06-19 14:01:07 +0700

    Added the "cropper_ratio" options for the ImageCollectionType.

--- Form/ImageCollectionType.php
+++ Form/ImageCollectionType.php
@@ -11,6 +11,10 @@
 
 namespace Glavweb\UploaderDropzoneBundle\Form;
 
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
 /**
  * Class ImageCollectionType
  *
@@ -25,5 +29,31 @@ class ImageCollectionType extends AbstractMediaCollectionType
     public function getBlockPrefix()
     {
         return 'cms_media_image_collection';
+    }
+
+    /**
+     * @param FormView $view
+     * @param FormInterface $form
+     * @param array $options
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+
+        $view->vars['cropperRatio'] = $options['cropper_ratio'];
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'compound'         => false,
+            'requestId'        => null,
+            'context'          => null,
+            'thumbnail_filter' => null,
+            'cropper_ratio'    => 1
+        ]);
     }
 }
